5c86ead5ae3eeb2fb2d393f685b2f3f72098006e
MariaTech2024 <mariavolzhina@gmail.com>
2024-05-28 20:51:05 +0300

    portfolio

---
+++ Portfolio/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: ['./src/**/*.{html,js}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+    }
+    
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
